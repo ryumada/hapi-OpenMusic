3dfba466bd859e979b918bd252513adb9a0bb4c1
ryumada <rizukiryumada@gmail.com>
2021-12-02 09:16:11 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log('Server berjalan pada %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
