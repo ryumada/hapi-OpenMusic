074b19b17f9d0d7c50a4f1f302fa0a853fc9ddf8
ryumada <rizukiryumada@gmail.com>
2021-12-09 15:29:39 +0700

    enhance SongsService to simplified the codes
    ~ get updatedAt value directly from insertedAt
    ~ fix getSongs() sql query to just SELECT needed attribute
    ~ using result.rowCount instead of result.rows.length

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -25,13 +25,12 @@ class SongsService {
   }) {
     const id = `song-${nanoid(16)}`;
     const insertedAt = new Date().toISOString();
-    const updatedAt = insertedAt;
 
     const query = {
       // eslint-disable-next-line max-len
-      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $7) RETURNING id',
       values: [
-        id, title, year, performer, genre, duration, insertedAt, updatedAt,
+        id, title, year, performer, genre, duration, insertedAt,
       ],
     };
 
@@ -49,7 +48,9 @@ class SongsService {
    * @return {object} mapped query result
    */
   async getSongs() {
-    const result = await this._pool.query('SELECT * FROM songs');
+    const result = await this._pool.query(
+        'SELECT id, title, performer FROM songs',
+    );
     return result.rows.map(mapSomeDBToModel);
   }
 
@@ -65,7 +66,7 @@ class SongsService {
     };
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError('Lagu tidak ditemukan');
     }
 
@@ -88,7 +89,7 @@ class SongsService {
 
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
     }
   }
@@ -105,7 +106,7 @@ class SongsService {
 
     const result = this._pool.query(query);
 
-    if (!(await result).rows.length) {
+    if (!(await result).rowCount) {
       throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
     }
   }
