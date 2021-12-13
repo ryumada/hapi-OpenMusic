7f08c9c1c65fced62509eca0624560a175a82482
ryumada <rizukiryumada@gmail.com>
2021-12-13 16:30:05 +0700

    created migration for playlists
    ~ fix typo in scenario file

---
+++ migrations/1639356012613_create-table-playlistsongs.js
@@ -0,0 +1,49 @@
+/* eslint-disable camelcase */
+/**
+ * Conjuction Table used to connect many-to-many relationship tables
+ */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlistsongs', {
+    id: {
+      type: 'VARCHAR(46)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(25)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(21)',
+      notNull: true,
+    },
+  });
+
+  /**
+   * Menambahkan constraint UNIQUE, kombinasi dari kolom playlist_id dan song_id
+   * Guna menghindari duplikasi data antara keduanya
+   */
+  pgm.addConstraint(
+      'playlistsongs',
+      'unique_playlist_id_and_song_id',
+      'UNIQUE(playlist_id, song_id)',
+  );
+
+  // kasih foreign key constraint untuk playlist_id dan song_id
+  pgm.addConstraint(
+      'playlistsongs',
+      'fk_playlistsongs.playlist_id_playlists.id',
+      'FOREIGN KEY(playlist_id) REFERENCES playlists(id)',
+  );
+  pgm.addConstraint(
+      'playlistsongs',
+      'fk_playlistsongs.song_id_songs.id',
+      'FOREIGN KEY(song_id) REFERENCES songs(id)',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlistsongs');
+};
